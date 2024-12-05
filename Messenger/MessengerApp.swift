385e82d19a03a31a099bffc4a9c13c76dcacf655
Ahmet Karadağ <akkaradaga@gmail.com>
2024-12-05 20:57:40 +0300

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by ahmet karadağ on 5.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
